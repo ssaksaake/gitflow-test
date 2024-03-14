59db355499defa90ac1933211f954d384acb6bd5
saessak <gkstotkr1201@gmail.com>
2024-03-14 16:23:16 +0900

    Design : Add first Page UI

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <h1>Git Flow Test Page</h1>
+            <div>Git Flow 트리 구조를 한번 만들어볼까??</div>
+        </main>
+    );
+}
