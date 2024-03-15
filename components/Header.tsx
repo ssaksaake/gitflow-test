1f7afa14bd320b30ebc8d684055148bdb16bcc81
saessak <gkstotkr1201@gmail.com>
2024-03-15 17:39:21 +0900

    Design : Fix header backgorund-color

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Header = () => {
     return (
-        <header style={{ width: "100%", height: "70px", backgroundColor: "yellow" }}>
+        <header style={{ width: "100%", height: "70px", backgroundColor: "green" }}>
             이거슨 헤더이다
         </header>
     );
