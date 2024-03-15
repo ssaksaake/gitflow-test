67e33ee4b24ef62b0a2836de264296aff02a04b5
saessak <gkstotkr1201@gmail.com>
2024-03-15 17:38:49 +0900

    Feat : Add header UI

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header style={{ width: "100%", height: "70px", backgroundColor: "yellow" }}>
+            이거슨 헤더이다
+        </header>
+    );
+};
