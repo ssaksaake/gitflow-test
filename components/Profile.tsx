56aaa7b34d8156d200b9de3099f182d2106b4d9a
saessak <gkstotkr1201@gmail.com>
2024-03-15 17:42:55 +0900

    Design : Add profile image

---
+++ components/Profile.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div style={{ width: "500px", height: "500px", backgroundColor: "yellow" }}>
+            <div>
+                <Image
+                    alt="프로필 이미지"
+                    src={
+                        "https://static.cdn.soomgo.com/upload/portfolio/58a10944-d6e4-46b1-90ab-6c706f6bd999.png?webp=1"
+                    }
+                />
+            </div>
+            새싹쿵야
+        </div>
+    );
+};
