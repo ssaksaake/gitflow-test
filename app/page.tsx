aecc69aca99618985bbbfa489f252482d3160444
saessak <gkstotkr1201@gmail.com>
2024-03-15 17:38:49 +0900

    Feat : Add header UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Header } from "@/components/Header";
 
 export default function Home() {
     return (
         <main className={styles.main}>
+            <Header />
             <h1>Git Flow Test Page</h1>
             <div>Git Flow 트리 구조를 한번 만들어볼까??</div>
         </main>
