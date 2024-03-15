302c20bf368d121cd81d99c451b4a2700d03269d
saessak <gkstotkr1201@gmail.com>
2024-03-15 17:41:32 +0900

    Feat : Add profile ui

---
+++ components/Profile.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div style={{ width: "500px", height: "500px", backgroundColor: "yellow" }}>
+            프로필 영역
+        </div>
+    );
+};
