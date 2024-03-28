ff0e8589116092ddc3f4aeda72958bb7fd16de54
saessak <gkstotkr1201@gmail.com>
2024-03-28 17:59:52 +0900

    sfsf

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <main className={styles.main}>
             <h1>Git Flow Test Page</h1>
-            <div>Git Flow 트리 구조를 한번 만들어볼까??</div>
+            <div>Git Flow 트리 구조를 한번 만들어볼까ㅇㄴㄹㄴㄹ??</div>
             <div>이거 급한거야!!!</div>
         </main>
     );
